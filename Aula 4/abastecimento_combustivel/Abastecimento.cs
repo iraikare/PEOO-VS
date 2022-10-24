63c05ad414eb7d3a7c6fe3de4dccc0818937b90e
Iraikare Rodrigues de Santana Alves Vieira <iraikare.rodrigues@academico.ifrn.edu.br>
2022-10-24 15:40:46 -0300

    upload de AbastecimentoApp

---
+++ Aula 4/abastecimento_combustivel/Abastecimento.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbastecimentoApp
+{
+    class Abastecimento
+    {
+        private double valorLitro, valorPago, KmRodados;
+        public void SetValorLitro(double valorLitro)
+        {
+            if (valorLitro > 0) this.valorLitro = valorLitro;
+        }
+        public void SetValorPago(double valorPago)
+        {
+            if (valorPago > 0) this.valorPago = valorPago;
+        }
+        public void SetKmRodados(double KmRodados)
+        {
+            if (KmRodados > 0) this.KmRodados = KmRodados;
+        }
+        public double MediaKmLitro()
+        {
+            double numLitros = valorPago / valorLitro;
+            return KmRodados / numLitros;
+        }
+        public double MediaReaisKm()
+        {
+            return valorPago / KmRodados;
+        }
+    }
+}
