52cd142bcbedae6d1bed68994828e531aae70229
Iraikare Rodrigues de Santana Alves Vieira <iraikare.rodrigues@academico.ifrn.edu.br>
2022-11-11 15:38:20 -0300

    BingoApp upload

--- Aula 5/BingoApp/MainWindow.xaml.cs
+++ Aula 5/BingoApp/MainWindow.xaml.cs
@@ -29,5 +29,34 @@ namespace BingoApp
         {
             txtNumBolas.Text = slider.Value.ToString();
         }
+        private Bingo b;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b = new Bingo();
+            b.Iniciar(int.Parse(txtNumBolas.Text));
+            btnIniciar.IsEnabled = false;
+            btnSortear.IsEnabled = true;
+            txtNum.Clear();
+            txtSorteados.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int x = b.Sortear();
+            if (x == -1)
+            {
+                txtNum.Text = "Fim";
+                btnIniciar.IsEnabled = true;
+                btnSortear.IsEnabled = false;
+            }
+            else
+            {
+                txtNum.Text = x.ToString();
+                string s = "";
+                foreach (int i in b.Sorteados())
+                    s = s + i + " - ";
+                txtSorteados.Text = s;
+            }
+        }
     }
 }
